0e673eb8520e0f5dfd0ef04e485f172cf8dc0e87
bmohitsrivastava07 <103405843+bmohitsrivastava07@users.noreply.github.com>
2023-05-19 18:02:03 +0530

    Add files via upload

---
+++ src/main/java/com/ArtGalleryManagement/Backend/Entity/Payment.java
@@ -0,0 +1,19 @@
+package com.ArtGalleryManagement.Backend.Entity;
+
+public class Payment {
+  private double amount;
+  private String userEmail;
+public double getAmount() {
+	return amount;
+}
+public void setAmount(double amount) {
+	this.amount = amount;
+}
+public String getUserEmail() {
+	return userEmail;
+}
+public void setUserEmail(String userEmail) {
+	this.userEmail = userEmail;
+}
+  
+}
