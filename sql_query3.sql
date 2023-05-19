e628aa9091a596d577287904bf5512ab0db4436d
bmohitsrivastava07 <103405843+bmohitsrivastava07@users.noreply.github.com>
2023-05-19 17:59:32 +0530

    Add files via upload

---
+++ sql_query3.sql
@@ -0,0 +1,76 @@
+USE `artgallerymanagement`;
+DROP TABLE IF EXISTS `product`;
+CREATE TABLE `product` (
+  `product_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  `artist` varchar(45) DEFAULT NULL,
+  `product_description` text DEFAULT NULL,
+  `quantities` int(11) DEFAULT NULL,
+  `quantity_available` int(11) DEFAULT NULL,
+  `category` varchar(11) DEFAULT NULL,
+  `image` MEDIUMBLOB  DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `review`;
+CREATE TABLE `review` (
+  `review_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `user_email` varchar(45) DEFAULT NULL,
+  `date` datetime(6) DEFAULT NULL,
+  `rating` decimal (3,2) DEFAULT NULL,
+  `product_id` BIGINT(20) DEFAULT NULL,
+  `review_description` text DEFAULT NULL,
+  PRIMARY KEY (`review_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `review` VALUES 
+	(1, 'example1user@email.com', NOW(), 4, 1, 'First book is pretty good book overall'),
+	(2, 'example2user@email.com', NOW(), 4.5, 2, 'Second books is pretty good book overall');
+    
+    
+DROP TABLE IF EXISTS `checkout`;
+CREATE TABLE `checkout` (
+  `checkout_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `user_email` varchar(45) DEFAULT NULL,
+  `checkout_date` varchar(45) DEFAULT NULL,
+  `return_date` varchar(45) DEFAULT NULL,
+  `product_id` BIGINT(20) DEFAULT NULL,
+  PRIMARY KEY (`checkout_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+INSERT INTO `checkout` VALUES 
+ 	(1, 'example1user@email.com', '2022-05-22', '2022-06-25', 1),
+ (2, 'example2user@email.com', '2022-05-22', '2022-06-26', 1),     (3, 'example1user@email.com', '2022-05-22', '2022-06-01', 2);
+    
+    
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE `messages` (
+  `message_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `user_email` varchar(45) DEFAULT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `question` text DEFAULT NULL,
+  `admin_email` varchar(45) DEFAULT NULL,
+  `response` text DEFAULT NULL,
+  `closed` tinyint(1) DEFAULT 0,
+  PRIMARY KEY (`message_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+ INSERT INTO `messages` VALUES  	(1, 'example3user@email.com', 'What should we do here?', 'I have been trying to work this out for a long time and no matter what happens I cannot figure this dang thing out', 'example1user@email.com', 'I do not know what to tell you friend. You are on your own.', 1),
+    (2, 'example2user@email.com', 'What should we do here example 2 user?', 'I have been trying to work this out for a long time and no matter what happens I cannot figure this dang thing out', 'example1user@email.com', 'I do not know what to tell you friend. You are on your own.',  1);
+    
+
+DROP TABLE IF EXISTS `history`;
+CREATE TABLE `history` (
+  `history_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `user_email` varchar(45) DEFAULT NULL,
+  `checkout_date` varchar(45) DEFAULT NULL,
+  `returned_date` varchar(45) DEFAULT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `artist` varchar(45) DEFAULT NULL,
+  `product_description` text DEFAULT NULL,
+  `image` MEDIUMBLOB  DEFAULT NULL,
+  PRIMARY KEY (`history_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+UNLOCK TABLES;
